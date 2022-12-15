e6e0f94f5f504fc5df4e63b45ea7d7df56e84d10
playBit1 <87233951+playBit1@users.noreply.github.com>
2022-12-15 15:58:21 +0530

    Delete pill.py

--- pill.py
+++
@@ -1,9 +0,0 @@
-class Pill:
-    def __init__(self, name, total_qty, before_meal, intake_qty, intake_time, intake_daily, selected_days=None):
-        self.name = name
-        self.total_qty = total_qty
-        self.before_meal = before_meal
-        self.intake_qty = intake_qty
-        self.intake_time = intake_time
-        self.intake_daily = intake_daily
-        self.selected_days = selected_days
