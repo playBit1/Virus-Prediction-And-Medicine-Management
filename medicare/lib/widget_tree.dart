91af0d9e1782d2705c8544a1bfc7f403a760d77f
playBit1 <binulben5@gmail.com>
2022-12-28 21:35:06 +0530

    flutter files modified for firebase

---
+++ medicare/lib/widget_tree.dart
@@ -0,0 +1,27 @@
+import 'package:medicare/authenticate.dart';
+import 'package:medicare/pages/home_page.dart';
+import 'package:medicare/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Authenticate().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
